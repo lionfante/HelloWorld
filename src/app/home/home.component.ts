0b11ff5ea1e11cca9103960c9e914822b0b60ae3
Thiên Phú <lionfante@gmail.com>
2022-12-03 14:59:09 +0700

    init code

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+  public name = "Phu";
+  public age = 20;
+  public vehicles = ["Toyota","Honda"];
+  //constructor(){ }
+}
